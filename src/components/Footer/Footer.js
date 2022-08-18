a70bfa4d1c78e9c12d69023097a3d8d9c0ba3a25
Chaedie <chaedong.im.dev@gmail.com>
2022-08-18 23:35:33 +0900

    Add: 하단 Footer 레이아웃 완성

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,14 +2,14 @@ import './Footer.scss';
 
 function Footer() {
   return (
-    <footer className="footer">
+    <div className="footer">
       <div className="menuContainer">
-        {MENU_LIST.map(menu => {
+        {MENU_LIST.map(({ title, iconSrc, detailedMenus }) => {
           return (
-            <div key={menu.title} className="menu">
+            <div key={title} className="menu">
               <div className="title">
-                <img className="menuIcon" src={menu.iconSrc} alt="icon" />
-                <h3>{menu.title}</h3>
+                <img className="menuIcon" src={iconSrc} alt="icon" />
+                <h3>{title}</h3>
                 <img
                   className="chevron"
                   src="/images/footer/icon-chevron.png"
@@ -17,7 +17,7 @@ function Footer() {
                 />
               </div>
               <ul>
-                {menu.detailedMenus.map(detailMenu => (
+                {detailedMenus.map(detailMenu => (
                   <li key={detailMenu}>
                     <a href="/">{detailMenu}</a>
                   </li>
@@ -27,7 +27,31 @@ function Footer() {
           );
         })}
       </div>
-    </footer>
+      <footer className="footerInfo">
+        <hr />
+        <div className="icons">
+          <img src="/images/footer/icon-instagram.png" alt="linkIcon" />
+          <img src="/images/footer/icon-facebook.png" alt="linkIcon" />
+          <img src="/images/footer/icon-phone.png" alt="linkIcon" />
+          <img
+            src="/images/footer/icon-github.png"
+            className="github"
+            alt="linkIcon"
+          />
+        </div>
+        <div className="infos">
+          <p className="bold">&copy; 따봉 가락신. 판권 소유.</p>
+          <p>
+            회사 이름: Alpargatas SA • CNPJ: 61.079.117/0001-05 • 주소: Av. of
+            the United Nations, nº 14.261 - Ala A - 10층 - São Paulo - São Paulo
+            - Brazil - CEP 04794-000 • CNPJ: 61.079.117/0001-05 연락처: SP (11)
+            3003-3414 | 기타 지역 0800 7070 566. 운영 시간: 월요일부터
+            금요일까지 오전 9시부터 오후 6시까지. 지불 옵션: 신용 카드(Visa,
+            MasterCard, Elo, Amex, Diners Club 및 Hipercard) 및 Boleto Bancário.
+          </p>
+        </div>
+      </footer>
+    </div>
   );
 }
 export default Footer;
