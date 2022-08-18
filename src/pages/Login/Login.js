bc22d9d33254fcfc68493d499ed976cfcc7c7bec
strongsongky <102295416+strongsongky@users.noreply.github.com>
2022-08-18 17:17:45 +0900

    login page layout

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,38 @@
+import React from 'react';
+import './Login.scss';
+
 function Login() {
-  return <h1>Login입니다.</h1>;
+  return (
+    <div className="login-field">
+      <div className="login-box">
+        <ul className="login-nav">
+          <li id="login-text" className="nav-item">
+            로그인
+          </li>
+          <li id="signup-text" className="nav-item">
+            계정만들기
+          </li>
+        </ul>
+        <form className="login-content">
+          <div className="email-form">
+            <label className="form-label">
+              <font>이메일</font>
+              <input className="input-email" />
+            </label>
+          </div>
+          <div className="passsword-form">
+            <label className="form-label">
+              <font>비밀번호</font>
+              <input className="input-password" />
+            </label>
+          </div>
+          <button type="submit" className="login-btn">
+            로그인
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
