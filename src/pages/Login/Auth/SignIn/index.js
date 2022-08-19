e354b505d3f27da70bb896adc5f3d41413ae6e1c
strongsongky <102295416+strongsongky@users.noreply.github.com>
2022-08-20 05:02:29 +0900

    input value onChange

--- src/pages/Login/Auth/SignIn/index.js
+++ src/pages/Login/Auth/SignIn/index.js
@@ -1,18 +1,30 @@
 import './signIn.scss';
+import { useState } from 'react';
 
 function LoginInput() {
+  const [inputs, setInputs] = useState({ name: '', password: '' });
+  const { name, password } = inputs;
+  const onChange = e => {
+    console.log(e.target.name);
+    console.log(e.target.value);
+  };
   return (
     <form className="login-content">
       <div className="email-form">
         <label className="form-label">
           이메일
-          <input type="text" className="input-email" />
+          <input name="name" onChange={onChange} className="input-email" />
         </label>
       </div>
       <div className="passsword-form">
         <label className="form-label">
           비밀번호
-          <input type="password" className="input-password" />
+          <input
+            name="password"
+            onChange={onChange}
+            type="password"
+            className="input-password"
+          />
         </label>
       </div>
       <button type="submit" className="login-btn">
