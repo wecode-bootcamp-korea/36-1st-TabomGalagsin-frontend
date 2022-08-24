530b535bacc70caeb109e5638189ea2fee801e97
pmb087 <pmb087@gmail.com>
2022-08-24 22:57:56 +0900

    FIX: SCSS Nesting

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
-import DetailPage from './pages/DetailPage/DetailPage';
 import Cart from './pages/Cart/Cart';
 import ProductsList from './pages/ProductsList/ProductsList';
 
@@ -12,7 +11,6 @@ function Router() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/detail" element={<DetailPage />} />
         <Route
           path="/category/:categoryId/type/:typeId"
           element={<ProductsList />}
